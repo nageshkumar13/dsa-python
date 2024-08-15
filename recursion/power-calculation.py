cae9f263bfb6973cb8a055b5b74bc9f9b9cade56
nageshkumar13 <nageshkumar1925@gmail.com>
2024-08-15 20:46:47 +0530

    added power-calculation file to folder

---
+++ recursion/power-calculation.py
@@ -0,0 +1,15 @@
+class Solution:
+    def calc_power(self, x, n):
+        if n == 0:
+            return 1
+        return x * self.calc_power(x, n-1)
+    
+
+solution = Solution()
+
+num = 5
+power = 3
+
+res = solution.calc_power(num, power)
+print(f"{num} to the power of {power} is {res} .")
+
